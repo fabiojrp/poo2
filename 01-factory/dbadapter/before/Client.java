078d819c4b7d0c80d6fc3ccc26e938ae66db0749
fabiojrp <fabio.pinheiro@ifc.edu.br>
2026-04-02 09:53:22 -0300

    ok

--- 01-factory/dbadapter/before/Client.java
+++ 01-factory/dbadapter/before/Client.java
@@ -7,8 +7,6 @@ public class Client {
 	public static void main(String[] args) {
 		OracleDB db = new OracleDB();
 		db.query("SELECT * FROM users");
-		db.update("INSERT INTO users VALUES ('User', 25)");
-		
-		//What happen if you have to change for a postgres database?
+		db.update("INSERT INTO users VALUES ('User', 25)");		
 	}
 }
